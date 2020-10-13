98dd40a2a021c9985512b4c22672b56d378d1ea4
PJ Lindsay <pjlindsay@gmail.com>
2020-10-13 10:36:11 -0600

    Add Player's Special Attack

--- README.md
+++ README.md
@@ -2,12 +2,22 @@
 
 Monster Slayer game
 
-## Concepts
-
 Inspired by Udemy Vue3 course
 
+
+## Game Rules
+
+Simple game to show off basic Vue.js functionality
+
 Player attack damage: between 5-12 HP per attack
+
+Player special attack: available every 3 turns
 
 Monster attack damage: between 8-15 HP per attack
 
 Player has healing potions
+
+## Concepts
+
+- Computed properties
+- Dynamic Style binding / Interpolation
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 
+// normal attack damage range
 const maxPlayerDamage = 12
 const minPlayerDamage = 6
+
+// special attack damage range
+const maxPlayerDamageSpecial = 25
+const minPlayerDamageSpecial = 10
 
 const maxMonsterDamage = 15
 const minMonsterDamage = 8
@@ -16,7 +21,19 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
+    }
+  },
+  computed: {
+    monsterHealthBarStyle() {
+      return { width: this.monsterHealth + '%' }
+    },
+    playerHealthBarStyle() {
+      return { width: this.playerHealth + '%' }
+    },
+    canUseSpecialAttack() {
+      return this.currentRound % 3 !== 0
     }
   },
   methods: {
@@ -26,19 +43,34 @@ const app = Vue.createApp({
     ** attack damage between 5-12 HP
     */
     attackMonster() {
+      this.currentRound++
       const attackDamage = getRandomDamageValue(minPlayerDamage, maxPlayerDamage)
       this.monsterHealth -= attackDamage
 
       // Monster's counter-attack
       this.attackPlayer()
     },
+
     /*
     ** Monster attacking player
     */
     attackPlayer() {
       const attackDamage = getRandomDamageValue(minPlayerDamage, maxPlayerDamage)
       this.playerHealth -= attackDamage
-    }
+    },
+
+    /*
+    ** Player's special attack on monster
+    */
+    specialAttackMonster() {
+      this.currentRound++
+      const attackDamage = getRandomDamageValue(minPlayerDamageSpecial, maxPlayerDamageSpecial)
+      this.monsterHealth -= attackDamage
+
+      // Monster's counter-attack
+      this.attackPlayer()
+    },
+
   }
 })
 
